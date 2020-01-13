c7cbe042275dc687fcdb95c7539100c5feb21ffe
huanghoujie18 <huanghoujie@163.com>
2020-01-13 21:48:16 +0800

    第二次提交代码

---
+++ test_unit1.py
@@ -0,0 +1,46 @@
+import pytest
+
+from unit.Div import div
+
+
+class TestDiv():
+
+    @pytest.mark.parametrize("num1,num2,expection", {
+        (4, 2, 2),
+        (0, 3, 0),
+        (1, 2, 0.5),
+        (100000000, 1, 100000000),
+        (-1, 2, -0.5),
+        (2, -1, -2),
+        (-9, -9, 1)
+    })
+    def test_div_int(num1, num2, expection,self):
+        assert div(num1, num2) == expection
+
+
+    @pytest.mark.parametrize("num1,num2,expection", {
+        (0.4, 2, 0.2),
+        (10, 0.1, 100),
+        (-1.5, 1, -1.5),
+        (100000000, -2, -50000000),
+    })
+    def test_div_float(num1, num2, expection,self):
+        assert div(num1, num2) == expection
+
+
+    @pytest.mark.parametrize("num1,num2,expection", [
+        (1, "a", "error"),
+        ("a", 2, "error"),
+        ("a", "b", "error"),
+
+    ])
+    def test_div_string(num1, num2, expection,self):
+        assert div(num1, num2) == expection
+
+
+    # 测试异常情况,如除数为0
+    @pytest.mark.parametrize("num1,num2,expection", [
+        (1, 0, "error")
+    ])
+    def test_div_exception(num1, num2, expection,self):
+        assert div(num1, num2) == expection
